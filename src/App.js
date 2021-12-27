ad9bb9d46afb42524917bccc2adf7ecf71443e61
Nowaybbq <filimonovworkdenis@gmail.com>
2021-12-27 16:23:40 +0200

    Add files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg"
+import "./App.css"
+import Search from "./Search/AutoComplete"
+
+function App() {
+  const ProgLang = ["Javascript", "Java", "Python", "Nodejs", "Reactjs"]
+  return (
+    <div className="container">
+      <Search lang={ProgLang} />
+    </div>
+  )
+}
+
+export default App
